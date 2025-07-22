d39c7cacb7832cc7dbebeeea6d5b8359d56b5283
Marc von Holzen <marc@vonholzen.org>
2025-07-22 11:47:52 -0700

    update logo, favicon and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Dropdown from './components/Dropdown/Dropdown';
-import ToggleComponent from './components/Toggle/Toggle';
 import Container from './components/Container/Container';
 // TODO: Add visual representation of a failed POST request, as a list of errors
 
@@ -10,6 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="absolute top-4 left-4 flex items-center gap-3">
+          <img src="/logo.png" alt="Logo" className="w-24 h-24" />
+          <h1 className="text-2xl font-bold text-black">play-editor</h1>
+        </div>
         <div className='flex flex-col gap-10 bg-white p-10'>
           <Container url="/api/v2/root" />
         </div>
